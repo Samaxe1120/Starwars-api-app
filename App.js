a25a6914e26b5f006dbb2b35aeecc9aebe93b336
Samaxe1120 <32623694+Samaxe1120@users.noreply.github.com>
2018-04-22 20:46:47 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css'
+import SearchBox from './SearchBox';
 import lightsaber from './lightsaber.svg'
+import {characters} from './characters.js'
+import {Card, Card2, Card3, Card4, Card5, Card6, cardComponent} from './Card';
 
-const App = () => {
-  return(
-    <div className="App">
-    <header className="App-header">
-          <img src={lightsaber} className="App-logo" alt="logo"/>
-          <h1 className="App-title">
 
-            Star Wars Character App</h1>
-    </header>  
-    </div>
-  )
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+  characters: characters,
+   searchfield: ''
 }
+    }
 
-export default App;
+        onSearchChange = (event) => {
+          this.setState({searchfield: event.target.value})
+        }   
+
+    render(){
+        const filteredCharacters = this.state.characters.filter(characters =>{
+ return characters.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+   })
+
+    return(
+      <div className="App">
+      <header className="App-header">
+            <img src={lightsaber} className="App-logo" alt="logo"/>
+            <h1 className="App-title">
+              Star Wars Character App</h1>
+      </header>  
+      <SearchBox searchChange= {this.onSearchChange} />
+      <cardComponent characters = {filteredCharacters}/>
+      
+      </div>
+    ); 
+  }
