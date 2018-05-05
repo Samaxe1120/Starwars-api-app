45092cad8d4be2c6b27c53b26ec0d7b971f14557
Samaxe1120 <32623694+Samaxe1120@users.noreply.github.com>
2018-05-04 21:21:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js'
+import CardList from './CardList'
 import {Card, Card2, Card3, Card4, Card5, Card6, cardComponent} from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import {characters} from './characters.js'
@@ -10,29 +11,7 @@ import {characters} from './characters.js'
 
 ReactDOM.render(
     <div>  
-    <App />
-    
-    <cardComponent/>,
-
-    <Card 
-       name= {characters[0].name}
-    />,
-
-    <Card2
-    name = {characters[1].name} 
-    />,
-    
-    <Card3 
-    name= {characters[2].name} />,
-    
-    <Card4
-    name = {characters[3].name}/>,
-    
-    <Card5
-    name= {characters[4].name}/>
-    
-    <Card6
-    name= {characters[5].name}/>
-
-       </div>, document.getElementById('root'));
+    <App/>
+      
+   , </div>, document.getElementById('root'));
 registerServiceWorker();
