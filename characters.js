ef37716b7fe2d36f68e4ced57c8349efdb4427d2
Samaxe1120 <32623694+Samaxe1120@users.noreply.github.com>
2018-05-04 21:22:29 -0400

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import luke from './luke.jpg'
 export const characters =[ 
         {
-    luke: <img alt='Luke Skywalker' src ='https://www.illustrationweb.com/ThumbSgs/105197/image_81621/luke-skywalker-character.jpg'/>,
+    pic1: <img alt='Luke Skywalker' src ={'https://www.illustrationweb.com/ThumbSgs/105197/image_81621/luke-skywalker-character.jpg'}/>,
     id: 1,
     people: 1,
     name: 'Luke.S'
 },
 
 { 
+    pic: <img alt="" src="https://i.pinimg.com/originals/30/38/24/303824ba0619d1d47a808b455dd14503.jpg"/>,
     people: 10,
     id:2,
     name: 'Obi Wan Kenobi'
@@ -16,24 +17,28 @@ export const characters =[
 },
 
 {
+    pic:  <img  alt = 'Master Yoda' src ='https://i.pinimg.com/236x/29/22/b9/2922b9ecbd47b6625b5f74b9ef52f9d2--star-wars-yoda-star-wars-fan-art.jpg'/>,
     people: 20,
     id:3,
     name:'Master Yoda'
 },
 
 {
+    pic: <img alt ='' src='http://media.moddb.com/images/groups/1/9/8071/darth_vader_by_eseagal-d5w9kag.jpg'/>,
     people: 4,
     id:4,
     name: 'Darth Vader'
 },
 
 {
+    pic: <img alt='' src='https://instagram.com/p/-IEXM_LtQT/media/?size=m'/>,
     people: 14,
     id:5,
     name: 'Han Solo'
 },
 
 {
+    pic: <img alt='' src='https://i.pinimg.com/originals/db/12/58/db1258a26112c36490fb5dee797fb7a7.jpg'/>,
     people: 22,
     id:6,
     name: 'Boba Fett'
