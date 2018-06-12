1f88ac3b4f9acd1279375b009c8534a44df69a8b
Samaxe1120 <32623694+Samaxe1120@users.noreply.github.com>
2018-06-12 18:08:33 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react'
 import './App.css'
-import SearchBox from './SearchBox';
 import lightsaber from './lightsaber.svg'
-import {characters} from './characters.js'
-import {Card} from './Card.js'
-import CardList from './CardList.js'
+import {Card, Card2, Card3, Card4,Card5, Card6, Card7, Card8, Card9, Card10} from './Card.js'
+
+import wrapper from './Wrapper.js'
 
 
 
@@ -12,39 +11,33 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-  characters: [],
+  jedi: [],
    searchfield: ''
 }
     }
-
-    componentDidMount(){
-      // fetch('https://swapi.co/api/people/1/')
-      // .then(response =>{
-      //   repsonse.json(); 
-      // })
-      // .then()
-      this.setState({characters:characters});
-    }
-
-        onSearchChange = (event) => {
-          this.setState({searchfield: event.target.value})
-        }   
-
+                       
     render(){
-        const filteredCharacters = this.state.characters.filter(characters =>{
- return characters.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-   })
-
-    return(
+ return (
       <div className="App">
       <header className="App-header">
-            <img src={lightsaber} className="App-logo" alt="logo"/>
+           
             <h1 className="App-title">
-              Star Wars Character App</h1>
+              Star Wars Character API</h1>
       </header>  
-      <SearchBox searchChange= {this.onSearchChange} />
-      <CardList characters = {filteredCharacters}/>
-      </div>
+          
+      <div className = 'allcards'>
+           <Card jedi = {this.filteredCharacters}/>
+            <Card2/>
+            <Card3/>
+            <Card4/>
+            <Card5/>
+            <Card6/>
+            <Card7/>
+            <Card8/>
+            <Card9/>
+            <Card10/>
+            </div>
+        </div>
     ); 
   }
 
